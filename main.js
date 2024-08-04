75c467ef9b68355734f786e39ed70dd22acdb4d1
huylquias <ssshuylquias@gmail.com>
2024-08-04 13:10:58 -0300

    grid de produtos

--- main.js
+++ main.js
@@ -4,3 +4,14 @@ document.addEventListener("DOMContentLoaded", (event) => {
     toggler.classList.toggle("clicked");
   });
 });
+
+document.getElementById('toggleLink').addEventListener('click', function(event){
+  event.preventDefault();
+  var section = document.getElementById('toggleSelection'); 
+  if (section.classList.contains('hidden')) { 
+    section.classList.remove('hidden');
+    section.scrollIntoView({ behavior: 'smooth' });
+  } else {
+    section.classList.add('hidden');
+  }
+})
