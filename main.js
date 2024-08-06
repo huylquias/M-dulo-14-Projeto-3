a6273432ddb3c3f1a4f249c7954d2e3a9b5d7d38
Santana <101057083+hrstan@users.noreply.github.com>
2024-08-06 20:52:54 -0300

    grid produtos em png

--- main.js
+++ main.js
@@ -5,13 +5,15 @@ document.addEventListener("DOMContentLoaded", (event) => {
   });
 });
 
-document.getElementById('toggleLink').addEventListener('click', function(event){
-  event.preventDefault();
-  var section = document.getElementById('toggleSelection'); 
-  if (section.classList.contains('hidden')) { 
-    section.classList.remove('hidden');
-    section.scrollIntoView({ behavior: 'smooth' });
-  } else {
-    section.classList.add('hidden');
-  }
-})
+document
+  .getElementById("toggleLink")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    var section = document.getElementById("toggleSelection");
+    if (section.classList.contains("hidden")) {
+      section.classList.remove("hidden");
+      section.scrollIntoView({ behavior: "smooth" });
+    } else {
+      section.classList.add("hidden");
+    }
+  });
